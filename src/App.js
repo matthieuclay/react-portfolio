6e28761423402ecaec8fe3372ef61c392602ce27
Matthieu Clay <matthieu.clay@outlook.fr>
2022-03-14 12:00:00 +0100

    :sparkles: add navigation component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Project1 from "./pages/Project1";
+import Project2 from "./pages/Project2";
+import Project3 from "./pages/Project3";
+import Project4 from "./pages/Project4";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Portfolio</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project-1" element={<Project1 />} />
+        <Route path="/project-2" element={<Project2 />} />
+        <Route path="/project-3" element={<Project3 />} />
+        <Route path="/project-4" element={<Project4 />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
