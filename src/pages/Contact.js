33445e5c34038e6babd11549bf2038c06892fbe5
Matthieu Clay <matthieu.clay@outlook.fr>
2022-03-14 15:30:19 +0100

    :art: improve contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,83 @@
 import React from "react";
+import ContactForm from "../components/ContactForm";
+import Logo from "../components/Logo";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import SocialNetwork from "../components/SocialNetwork";
+import Buttons from "../components/Buttons";
+import { motion } from "framer-motion";
 
 const Contact = () => {
+  const pageTransition = {
+    in: {
+      opacity: 1,
+      x: 0,
+    },
+    out: {
+      opacity: 0,
+      x: 200,
+    },
+  };
+
   return (
-    <div className="Contact">
-      <h1>Contact</h1>
-    </div>
+    <main>
+      <Mouse />
+      <motion.div
+        className="contact"
+        initial="out"
+        animate="in"
+        exit="out"
+        variants={pageTransition}
+        transition={{ duration: 0.4 }}
+      >
+        <Navigation />
+        <Logo />
+        <ContactForm />
+        <div className="contact-infos">
+          <div className="address">
+            <div className="content">
+              <h4>address</h4>
+              <p>123 Street</p>
+              <p>59000, Lille</p>
+            </div>
+          </div>
+          <div className="phone">
+            <div className="content">
+              <h4>phone</h4>
+              <CopyToClipboard text="0770023839" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("Phone copied!")}
+                >
+                  +33 7 70 02 38 39
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <div className="email">
+            <div className="content">
+              <h4>email</h4>
+              <CopyToClipboard text="contact@graap.fr" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("Email copied!")}
+                >
+                  contact@graap.fr
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <SocialNetwork />
+          <div className="credits">
+            <p>Graap Web Agency - 2022</p>
+          </div>
+        </div>
+        <Buttons left={"/project-4"} />
+      </motion.div>
+    </main>
   );
 };
 
